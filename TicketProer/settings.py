1c4fec8bfbddfe80c15e35a839c2484b211dab41
Shota <shotaik@mail.ru>
2019-08-26 17:45:08 +0300

    Init docker

--- TicketProer/settings.py
+++ TicketProer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_env('SECRET_KEY', 'abccd')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env('DEBUG', True, bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
