b6e7c933b30b1652af65b23f738e46a69ce641f2
Shota <shotaik@mail.ru>
2019-09-05 14:09:53 +0300

    Add logout

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import re_path, path, include
-from rest_auth.registration.views import RegisterView, LoginView
+from rest_auth.registration.views import RegisterView
+from rest_auth.views import LogoutView, LoginView
 
 
 authpatterns = [
     path('registration/', RegisterView.as_view()),
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
 
 
