acbd5da95e47c4cab7ba4e865c41e31b67727ffa
Shota <shotaik@mail.ru>
2019-09-11 12:14:57 +0300

    Fix login test, add logout test

--- apps/api/tests/test_auth.py
+++ apps/api/tests/test_auth.py
@@ -21,17 +21,19 @@ class TestAuth:
         user = User.objects.get(email=user_info['email'])
         assert user.email == user_info['email']
         assert user.username == user_info['username']
-        assert user.password != user_info['password']
         assert user.first_name == user_info['first_name']
         assert user.last_name == user_info['last_name']
         assert user.is_staff is False
 
-    def test_login(self, client, user):
-        login_info = {
-            'email': user.email,
-            'password': user.password,
-        }
-        print(user.__dict__)
-        print(User.objects.all()[0].__dict__)
-        res = client.post('/api/auth/login/', data=login_info)
+    def test_login(self, client, user_dict):
+        user = User.objects.create_user(**user_dict)
+        res = client.post('/api/auth/login/', data={'email': user_dict['email'], 'password': user_dict['password']})
         assert res.status_code == status.HTTP_200_OK
+        assert len(res.json()) == 1
+        assert str(user.auth_token) == res.json()['key']
+
+    def test_logout(self, client, user, token):
+        res = client.post('/api/auth/logout/', **{'HTTP_AUTHORIZATION': 'Token ' + str(token)})
+        assert res.status_code == status.HTTP_200_OK
+        assert len(res.json()) == 1
+        assert res.json()['detail'] == 'Successfully logged out.'
