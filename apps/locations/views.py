9e2d8471916f48923e6c885f1191e3b1e803a8de
Shota <shotaik@mail.ru>
2019-09-16 12:56:58 +0300

    Add place crud

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from apps.locations.models import Place
+from tools.action_based_permission import ActionBasedPermission
 from apps.locations.serializers import PlaceSerializer
 
 
-class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
+class PlaceViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for listing and retrieving Places
+    """
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
+    permission_classes = (ActionBasedPermission,)
+    action_permissions = {
+        AllowAny: ['retrieve', 'list'],
+        IsAdminUser: ['destroy', 'create', 'update', 'partial_update'],
+    }
